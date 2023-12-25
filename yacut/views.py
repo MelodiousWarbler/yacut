376ea86c6c4215e1c6be48bb1c64967bc19dcf68
MelodiousWarbler <63454948+MelodiousWarbler@users.noreply.github.com>
2023-12-25 19:02:53 +0300

    f_review

--- yacut/views.py
+++ yacut/views.py
@@ -18,9 +18,10 @@ def index_view():
         return render_template(INDEX, form=form)
     try:
         url = URLMap.save(original=form.original_link.data, short=form.custom_id.data)
+        render=url_for(REDIRECT_VIEW, short=url.short, _external=True)
         return render_template(
             INDEX,
-            url=url_for(REDIRECT_VIEW, short=url.short, _external=True),
+            url=render,
             form=form
         )
     except InvalidAPIUsage:
